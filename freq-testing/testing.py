e700f262cbfabdfc4fdf02ec27342ef8bbbe405e
Shravan Mandava <shravan@mandava8.com>
2025-10-21 17:56:28 +0100

    Remove Testing files

--- freq-testing/testing.py
+++
@@ -1,4 +0,0 @@
-with open("./cipher-rs/data/words.txt", "r") as words:
-    data = "".join([line.rstrip() for line in words]).rstrip()
-    eval("words = {}".format(data))
-    # eval("print('hellow')")
